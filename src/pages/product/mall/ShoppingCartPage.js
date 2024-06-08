bc8caa08260c863a9985e6964e92453f8309ba9e
TLectronic <3051732820@qq.com>
2024-06-08 12:48:46 +0800

    购物车页面更新

--- src/pages/product/mall/ShoppingCartPage.js
+++ src/pages/product/mall/ShoppingCartPage.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Menu } from "antd";
+import { Menu, Card, Button } from "antd";
+import { AppstoreOutlined, HomeOutlined, ShoppingCartOutlined, UserOutlined } from '@ant-design/icons';
+import './style.css';
 
 const ShoppingCartPage = () => {
   // 页面最下方菜单栏数据
@@ -7,29 +9,42 @@ const ShoppingCartPage = () => {
     {
       label: '首页',
       key: 'homepage',
-      // icon: <MailOutlined />,
+      icon: <HomeOutlined />,
     },
     {
       label: '分类',
       key: 'classification',
-      // icon: <AppstoreOutlined />,
+      icon: <AppstoreOutlined />,
     },
     {
       label: '购物车',
       key: 'shoppingcart',
-      // icon: <AppstoreOutlined />,
+      icon: <ShoppingCartOutlined />,
     },
     {
       label: '我的',
       key: 'personalinformation',
-      // icon: <AppstoreOutlined />,
+      icon: <UserOutlined />,
     },
   ];
 
 
   return (
-    <div>
-      <Menu items={menuItems} defaultSelectedKeys={['shoppingcart']} mode="horizontal" />
+    <div className="shopping-cart-page">
+      {/* 页面内容 */}
+      <div className="content">
+        <h2 className="titlePosition">购物车</h2>
+        <Card className="cardStyle">
+          <div className="cardContainer">
+            <Button type="primary" danger className="redButtonStyle">清空</Button>
+            <div>￥5098元</div>
+            <Button type="primary" className="blueButtonStyle">去结算</Button>
+          </div>
+        </Card>
+
+      </div>
+      <Menu items={menuItems} defaultSelectedKeys={['shoppingcart']} mode="horizontal" className="bottom-menu" />
+
     </div>
   );
 };
