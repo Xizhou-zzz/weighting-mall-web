2ab8ede28cf68374a130908d2191099f1cbc12c9
TLectronic <3051732820@qq.com>
2024-06-09 20:22:04 +0800

    个人信息页面基本完成

--- src/pages/product/mall/PersonalInformationPage.js
+++ src/pages/product/mall/PersonalInformationPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Menu, Card, Button, Image, InputNumber, Checkbox } from "antd";
-import { AppstoreOutlined, HomeOutlined, ShoppingCartOutlined, UserOutlined, MessageOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu, Card, Button, Avatar, Divider } from "antd";
+import { AppstoreOutlined, HomeOutlined, ShoppingCartOutlined, UserOutlined, MessageOutlined, SettingOutlined, ProfileOutlined, MoneyCollectOutlined, TruckOutlined, TransactionOutlined, EnvironmentOutlined, RightOutlined, HeartOutlined, StarOutlined, CommentOutlined, ClockCircleOutlined } from '@ant-design/icons';
 import './style.css';
 
 const PersonalInformationPage = () => {
@@ -31,7 +31,90 @@ const PersonalInformationPage = () => {
   return (
     <div className="shopping-cart-page">
       <div className="content">
+        <div className="buttonPosition">
+          <Button type="link" icon={<MessageOutlined />}></Button>
+          <Button type="link" icon={<SettingOutlined />}></Button>
+        </div>
+        <div style={{ display: "flex", alignItems: "center", position: 'relative', left: '5%', top: '1%' }}>
+          <Avatar src='/avatar.jpg' size={64}></Avatar>
+          <p>Tlectronic</p>
+        </div>
+        <p style={{ position: 'relative', left: '8%', top: '1%', color: 'gray' }}>白银会员</p>
+        <Button type="text" style={{ position: 'relative', left: '70%', top: '-6%' }}>立即开通</Button>
+        <Divider style={{ position: 'relative', top: '-5%' }} />
+
+        <Card style={{ position: 'relative', top: '-5%', width: '94%', left: '3%' }}>
+          <div style={{ display: 'flex' }}>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <p style={{ fontSize: '18px' }} >5000</p>
+              <p style={{ color: 'gray' }} >积分</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <p style={{ fontSize: '18px' }} >1000</p>
+              <p style={{ color: 'gray' }} >成长值</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <p style={{ fontSize: '18px' }} >暂无</p>
+              <p style={{ color: 'gray' }} >优惠券</p>
+            </div>
+          </div>
+        </Card>
+
+        <Card style={{ position: 'relative', top: '-3%', width: '94%', left: '3%' }}>
+          <div style={{ display: 'flex' }}>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <ProfileOutlined />
+              <p>全部订单</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <MoneyCollectOutlined />
+              <p>待付款</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <TruckOutlined />
+              <p>待收货</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <TransactionOutlined />
+              <p>退款/售后</p>
+            </div>
+          </div>
+        </Card>
+
+        <Card style={{ position: 'relative', top: '-1%', width: '94%', left: '3%' }}>
+          <div>
+            <EnvironmentOutlined />
+            地址管理
+            <RightOutlined />
+          </div>
+          <Divider />
+          <div>
+            <ClockCircleOutlined />
+            我的足迹
+            <RightOutlined />
+          </div>
+          <Divider />
+          <div>
+            <HeartOutlined />
+            我的关注
+            <RightOutlined />
+          </div>
+          <Divider />
+          <div>
+            <StarOutlined />
+            我的收藏
+            <RightOutlined />
+          </div>
+          <Divider />
+          <div>
+            <CommentOutlined />
+            我的评价
+            <RightOutlined />
+          </div>
+          <Divider />
+        </Card>
       </div>
+
       <Menu items={menuItems} defaultSelectedKeys={['personalinformation']} mode="horizontal" className="bottom-menu" />
     </div>
   );
